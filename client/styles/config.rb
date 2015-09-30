8adc3f62987a2591cb0127f35a11b317649d674e
Viengo <viengooleg@gmail.com>
2015-09-30 15:04:24 +0300

    helvetica

--- client/styles/config.rb
+++ client/styles/config.rb
@@ -2,13 +2,13 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = ""
+http_path = ".."
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "../images"
 http_generated_images_path = '../../images'
 javascripts_dir = "js"
-fonts_dir = "/../fonts"
+fonts_dir = "../fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
